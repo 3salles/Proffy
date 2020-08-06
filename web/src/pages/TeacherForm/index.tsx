f8a7a56d5626d878d1d8c134bd140ad8684fd9e7
Beatriz Salles <beatrizsallesss@gmail.com>
2020-08-06 14:44:06 -0300

    List teachers

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,10 +1,86 @@
 import React from "react";
 import PageHeader from "../../components/PageHeader";
+import Input from "../../components/Input";
+import Textarea from '../../components/TextArea';
+import Select from '../../components/Select';
+import warningIcon from '../../assets/images/icons/warning.svg';
+import './styles.css';
 
 function TeacherForm() {
   return (
     <div id="page-teacher-form" className="container">
-      <PageHeader title="Que incrível que você quer dar aulas." />
+      <PageHeader title="Que incrível que você quer dar aulas." 
+      description="O primeiro passo é preencher esse formulário de inscrição"
+      />
+
+      <main>
+        <fieldset>
+          <legend>Seus dados</legend>
+          <Input name="name" label="Nome Completo" />
+          <Input name="avatar" label="Avatar" />
+          <Input name="whatsapp" label="WhatsApp" />
+          <Textarea name="bio" label="Biografia"/>
+        </fieldset>
+
+        <fieldset>
+          <legend>Sobre a aula</legend>
+          <Select 
+          name="subject" 
+          label="Matéria"
+          options={[
+            {value: 'Artes', label: 'Artes'},
+            {value: 'Biologia', label: 'Biologia'},
+            {value: 'Ciências', label: 'Ciências'},
+            {value: 'Educação Física', label: 'Educação Física'},
+            {value: 'Física', label: 'Física'},
+            {value: 'Geografia', label: 'Geografia'},
+            {value: 'História', label: 'História'},
+            {value: 'Português', label: 'Português'},
+            {value: 'Matemática', label: 'Matemática'},
+            {value: 'Química', label: 'Química'},
+          ]}
+          ></Select>
+          <Input name="cost" label="Custo da dua hora por aula" />
+        </fieldset>
+
+        <fieldset>
+          <legend>
+            Horários Disponíveis
+            <button type="button">
+              + Novo horário
+            </button>
+          </legend>
+
+          <div className="schedule-item">
+            <Select 
+            name="week_day" 
+            label="Dia da semana"
+            options={[
+              {value: '0', label: 'Domingo'},
+              {value: '1', label: 'Segunda-feira'},
+              {value: '2', label: 'Terça-feira'},
+              {value: '3', label: 'Quarta-feira'},
+              {value: '4', label: 'Quinta-feira'},
+              {value: '5', label: 'Sexta-feira'},
+              {value: '6', label: 'Sábado'},
+            ]}>
+            </Select>
+            <Input name="from" label= "Das" type="time"/>
+            <Input name="to" label="Até" type="time"/>
+          </div>
+          
+        </fieldset>
+
+        <footer>
+          <p>
+            <img src={warningIcon} alt="Aviso Importante"/>
+            Importante! <br/>
+            Preencha todos os dados
+          </p>
+          <button type="button">Salvar Cadastro</button>
+        </footer>
+      </main>
+
     </div>
   );
 }
